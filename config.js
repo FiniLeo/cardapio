a2a496d1443dfc2f8e6f111ecc94d01f4e86b803
FiniLeo <fini_leo@hotmail.com>
2025-07-26 21:45:42 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,44 @@
+/**
+ * ====================================================================
+ * CONFIGURAÇÃO DO CARDÁPIO SHEETS - V3.7   - 24/07/2025 - 16h15    
+ * ====================================================================
+ * 
+ * Este arquivo contém todas as configurações principais do sistema,
+ * incluindo URLs das planilhas e textos traduzíveis.
+ */
+
+// ====================================================================
+// URLs DAS PLANILHAS DO GOOGLE SHEETS
+// ====================================================================
+/**
+ * Para alterar as planilhas utilizadas pelo cardápio, modifique as URLs abaixo.
+ * Certifique-se de que as planilhas estejam publicadas e acessíveis publicamente.
+ * 
+ * Como obter a URL correta:
+ * 1. Abra sua planilha no Google Sheets
+ * 2. Vá em Arquivo > Publicar na web
+ * 3. Selecione a aba específica que deseja publicar
+ * 4. Escolha o formato CSV
+ * 5. Copie a URL gerada
+ */
+
+//---LINKS DE DEMONSTRAÇÃO ABAIXO: TROQUE PELO SEUS LINKS
+
+// URL of the spreadsheet with menu items (tab: Items)
+const MENU_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSt7rH2QwIa76Nf-TzKYQocKCavaXfEwFe1zynpVwC6xGLTcP--tfWspFkiNLuOXAzm0eKMBmbf5Lc5/pub?gid=0&single=true&output=csv';
+
+// URL of the spreadsheet with menu categories (tab: Categories)
+const CATEGORIES_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSt7rH2QwIa76Nf-TzKYQocKCavaXfEwFe1zynpVwC6xGLTcP--tfWspFkiNLuOXAzm0eKMBmbf5Lc5/pub?gid=909003511&single=true&output=csv';
+
+// URL of the spreadsheet with general settings (tab: Config)
+const CONFIG_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSt7rH2QwIa76Nf-TzKYQocKCavaXfEwFe1zynpVwC6xGLTcP--tfWspFkiNLuOXAzm0eKMBmbf5Lc5/pub?gid=514993013&single=true&output=csv';
+
+// URL of the spreadsheet with operating hours (tab: Hours)
+const HOURS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSt7rH2QwIa76Nf-TzKYQocKCavaXfEwFe1zynpVwC6xGLTcP--tfWspFkiNLuOXAzm0eKMBmbf5Lc5/pub?gid=1943311669&single=true&output=csv';
+
+// URL of the spreadsheet with neighborhoods and delivery fees (tab: Bairros) **OPCIONAL**
+const NEIGHBORHOODS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSt7rH2QwIa76Nf-TzKYQocKCavaXfEwFe1zynpVwC6xGLTcP--tfWspFkiNLuOXAzm0eKMBmbf5Lc5/pub?gid=1101571404&single=true&output=csv';
+
+// URL da planilha de cupons de desconto
+const COUPONS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSt7rH2QwIa76Nf-TzKYQocKCavaXfEwFe1zynpVwC6xGLTcP--tfWspFkiNLuOXAzm0eKMBmbf5Lc5/pub?gid=1595472760&single=true&output=csv';
+
